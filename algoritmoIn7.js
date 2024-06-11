146edcbda7d9602292bf3d4d9c9eeb4c2df22698
Ana Clara <ana_goudinho@estudante.sesisenai.org.br>
2024-06-11 08:01:39 -0300

    fim2

--- algoritmoIn7.js
+++ algoritmoIn7.js
@@ -4,7 +4,7 @@ let resposta2 = document.getElementById('resposta2')
 
 function calcula(){
     // Resolvendo o problema com a equação
-    let real = 27/3.5
+    let real = ((27/3.5) + (39/6.3)) / 2
     console.log("Resposta:",real)
 
     // Saída dos dados para o usuário
@@ -13,15 +13,4 @@ function calcula(){
     resposta.style.fontSize = "24px"
     resposta.style.color = "white"
     resposta.style.fontFamily = "serif"
-    
-    // Resolvendo o problema com a equação
-    let real2 = 39/6.3
-    console.log("Resposta:",real2)
-    
-    // Saída dos dados para o usuário
-    
-    resposta2.innerHTML = "Resultado = " +real2.toFixed(2) + " km"
-    resposta2.style.fontSize = "24px"
-    resposta2.style.color = "white"
-    resposta2.style.fontFamily = "serif"
 }
